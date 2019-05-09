a8495783ff0d692648752f568ee7407ba342efc4
roman01la <roman01la@romanliutikov.com>
2019-05-09 17:39:03 +0300

    docs

--- benchmark/uix/benchmark.clj
+++
@@ -1,9 +0,0 @@
-(ns uix.benchmark)
-
-(defmacro bench [k iters expr]
-  `(let [start# (js/Date.now)
-         ret# (dotimes [_# ~iters] ~expr)
-         end# (js/Date.now)
-         elapsed# (- end# start#)
-         rate# (js/Math.round ( * (/ ~iters elapsed#) 1000))]
-     (println (str ~(name k) " x " rate# " ops/s, elapsed " elapsed# "ms"))))
