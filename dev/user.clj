dbeed20468c4e7d062ca2877c1f8e5712a765e51
roman01la <roman01la@romanliutikov.com>
2019-05-15 20:22:36 +0300

    configure ci

--- dev/user.clj
+++ dev/user.clj
@@ -1,20 +1,20 @@
-(ns user)
+(ns user
+  (:require [figwheel.main.api]
+            [codox.main]
+            [cljfmt.main]
+            [clojure.java.io])
+  (:import (java.io File)))
 
 (defn start []
-  (require 'figwheel.main.api)
   (figwheel.main.api/start "dev"))
 
 (defn docs []
-  (require 'codox.main)
   (codox.main/generate-docs
    {:metadata {:doc/format :markdown}
     :themes [:rdash]}))
 
 (defn fmt []
-  (require 'cljfmt.main)
-  (require 'clojure.java.io)
-  (import (java.io File))
   (let [paths (->> ["src" "dev"]
                    (map clojure.java.io/file)
-                   (filter #(and (.exists ^java.io.File %) (.isDirectory ^java.io.File %))))]
+                   (filter #(and (.exists ^File %) (.isDirectory ^File %))))]
     (cljfmt.main/fix paths {:file-pattern #"\.clj[cs]?$"})))
