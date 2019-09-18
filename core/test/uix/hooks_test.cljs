f9c3f165b10406ab28e58d8f92a50f3142a7de98
roman01la <roman01la@romanliutikov.com>
2019-09-18 23:14:18 +0300

    more tests

--- core/test/uix/hooks_test.cljs
+++ core/test/uix/hooks_test.cljs
@@ -1,15 +1,41 @@
 (ns uix.hooks-test
-  (:require [clojure.test :refer [deftest is testing run-tests]]
-            [uix.hooks.alpha :as hooks]
+  (:require [clojure.test :refer [deftest is testing run-tests async]]
+            [uix.hooks.alpha :as hooks :refer [maybe-js-deps maybe-ret-fn]]
+            [uix.dom.alpha :as dom]
             [uix.test-utils :as t]))
 
-(defn f-state []
-  (let [state (hooks/state 1)]
-    (is (instance? hooks/StateHook state))
-    (is (= @state 1))))
+(defn render [el]
+  (dom/render el js/root))
 
-(deftest state-hook
-  (t/as-string [f-state]))
+(deftest test-state-hook
+  (let [f-state (fn [done]
+                  (let [state (hooks/state 1)]
+                    (is (instance? hooks/StateHook state))
+                    (is (or (== @state 1) (== @state 2)))
+                    (if (== @state 2)
+                      (done)
+                      (swap! state inc))))]
+    (async done
+      (render [f-state done]))))
+
+(deftest test-ref-hooj
+  (let [f-ref (fn [done]
+                (let [ref (hooks/ref 1)]
+                  (is (instance? hooks/RefHook ref))
+                  (is (== @ref 1))
+                  (swap! ref inc)
+                  (is (== @ref 2))
+                  (done)))]
+    (async done
+      (render [f-ref done]))))
+
+(deftest test-maybe-js-deps
+  (is (.isArray js/Array (maybe-js-deps [])))
+  (is (= (maybe-js-deps nil) js/undefined)))
+
+(deftest test-maybe-ret-fn
+  (is (== 1 ((maybe-ret-fn (constantly 1)))))
+  (is (== js/undefined ((maybe-ret-fn (constantly nil))))))
 
 (defn -main []
   (run-tests))
