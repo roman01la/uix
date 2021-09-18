5156ad2b322bfd684e03bbf911853d28e8e55700
roman01la <roman01la@romanliutikov.com>
2021-09-18 13:39:39 +0300

    do not emit interpret call for nil attrs

--- core/src/uix/compiler/aot.clj
+++ core/src/uix/compiler/aot.clj
@@ -20,17 +20,18 @@
         id-class (attrs/parse-tag tag)
         tag (first id-class)
         m (meta v)
-        attrs (if-not (map? attrs)
-                `(uix.compiler.attributes/interpret-attrs ~attrs (cljs.core/array ~@id-class) false)
-                (cond-> attrs
-                  :always (attrs/set-id-class id-class)
-                  (:key m) (assoc :key (:key m))
-                  (:ref attrs) (assoc :ref `(uix.compiler.alpha/unwrap-ref ~(:ref attrs)))
-                  (and (some? (:style attrs))
-                       (not (map? (:style attrs))))
-                  (assoc :style `(uix.compiler.attributes/interpret-attrs ~(:style attrs) (cljs.core/array) true))
-                  :always (attrs/compile-attrs {:custom-element? (re-find #"-" tag)})
-                  :always js/to-js))
+        attrs (when (some? attrs)
+                (if-not (map? attrs)
+                  `(uix.compiler.attributes/interpret-attrs ~attrs (cljs.core/array ~@id-class) false)
+                  (cond-> attrs
+                    :always (attrs/set-id-class id-class)
+                    (:key m) (assoc :key (:key m))
+                    (:ref attrs) (assoc :ref `(uix.compiler.alpha/unwrap-ref ~(:ref attrs)))
+                    (and (some? (:style attrs))
+                         (not (map? (:style attrs))))
+                    (assoc :style `(uix.compiler.attributes/interpret-attrs ~(:style attrs) (cljs.core/array) true))
+                    :always (attrs/compile-attrs {:custom-element? (re-find #"-" tag)})
+                    :always js/to-js)))
         ret `(>el ~tag ~attrs ~@children)]
     ret))
 
@@ -42,24 +43,26 @@
 (defmethod compile-element :fragment [v]
   (let [[_ attrs & children] v
         m (meta v)
-        attrs (if-not (map? attrs)
-                `(uix.compiler.attributes/interpret-attrs ~attrs (cljs.core/array) false)
-                (cond-> attrs
-                  (:key m) (assoc :key (:key m))
-                  :always attrs/compile-attrs
-                  :always js/to-js))
+        attrs (when (some? attrs)
+                (if-not (map? attrs)
+                  `(uix.compiler.attributes/interpret-attrs ~attrs (cljs.core/array) false)
+                  (cond-> attrs
+                    (:key m) (assoc :key (:key m))
+                    :always attrs/compile-attrs
+                    :always js/to-js)))
         ret `(>el fragment ~attrs ~@children)]
     ret))
 
 (defmethod compile-element :suspense [v]
   (let [[_ attrs & children] v
         m (meta v)
-        attrs (if-not (map? attrs)
-                `(uix.compiler.attributes/interpret-attrs ~attrs (cljs.core/array) false)
-                (cond-> attrs
-                  (:key m) (assoc :key (:key m))
-                  :always attrs/compile-attrs
-                  :always js/to-js))
+        attrs (when (some? attrs)
+                (if-not (map? attrs)
+                  `(uix.compiler.attributes/interpret-attrs ~attrs (cljs.core/array) false)
+                  (cond-> attrs
+                    (:key m) (assoc :key (:key m))
+                    :always attrs/compile-attrs
+                    :always js/to-js)))
         ret `(>el suspense ~attrs ~children)]
     ret))
 
@@ -72,13 +75,14 @@
 (defmethod compile-element :interop [v]
   (let [[tag attrs & children] v
         m (meta v)
-        attrs (if-not (map? attrs)
-                `(uix.compiler.attributes/interpret-attrs ~attrs (cljs.core/array) true)
-                (cond-> attrs
-                  (:key m) (assoc :key (:key m))
-                  (:ref attrs) (assoc :ref `(uix.compiler.alpha/unwrap-ref ~(:ref attrs)))
-                  :always attrs/compile-attrs
-                  :always js/to-js))]
+        attrs (when (some? attrs)
+                (if-not (map? attrs)
+                  `(uix.compiler.attributes/interpret-attrs ~attrs (cljs.core/array) true)
+                  (cond-> attrs
+                    (:key m) (assoc :key (:key m))
+                    (:ref attrs) (assoc :ref `(uix.compiler.alpha/unwrap-ref ~(:ref attrs)))
+                    :always attrs/compile-attrs
+                    :always js/to-js)))]
     `(>el ~tag ~attrs ~children)))
 
 (defn compile-html
