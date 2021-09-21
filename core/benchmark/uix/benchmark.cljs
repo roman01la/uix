38816611f92dec4d01a42318d0452d85f9946a24
roman01la <roman01la@romanliutikov.com>
2021-09-21 21:00:35 +0300

    update benchmark

--- core/benchmark/uix/benchmark.cljs
+++ core/benchmark/uix/benchmark.cljs
@@ -3,38 +3,21 @@
   (:require [reagent.core :as r]
             ["react-dom/server" :as rserver]
             [react :as react]
-            [uix.compiler.alpha :as uixc]
-            [uix.core.alpha :refer [html]]
-            [uix.dom.alpha :as uix.dom]
             [uix.hiccup :as hiccup]
             [uix.react :refer [Editor]]))
 
-(defn reagent-interpret []
-  (r/as-element [hiccup/editor]))
-
-(defn uix-interpret []
-  (uixc/as-element [hiccup/editor]))
-
-(defn uix-compile []
-  (uixc/as-element [hiccup/editor-compiled]))
-
-
 (defn render [el]
-  (rserver/renderToString el))
+      (rserver/renderToString el))
 
-(do
+(def reagent-compiler
+  (r/create-compiler {:function-components true}))
 
-  (bench :react 10000 (render (react/createElement Editor)))
-  (bench :react 10000 (render (react/createElement Editor)))
+(js/console.log "Warming up...")
+(bench :react 10000 (render (react/createElement Editor)))
+(bench :uix-compiled 10000 (render #el [hiccup/editor-compiled]))
+(bench :reagent-interpret 10000 (render (r/as-element [hiccup/editor])))
 
-  (bench :uix-compile 10000 (render (uix-compile)))
-  (bench :uix-compile 10000 (render (uix-compile)))
-
-  (bench :uix-interpret 10000 (render (uix-interpret)))
-  (bench :uix-interpret 10000 (render (uix-interpret)))
-
-  (bench :reagent-interpret 10000 (render (reagent-interpret)))
-  (bench :reagent-interpret 10000 (render (reagent-interpret))))
-
-(uix.dom/render [uix-interpret] js/root)
-
+(js/console.log "Running the benchmark...")
+(bench :react 10000 (render (react/createElement Editor)))
+(bench :uix-compiled 10000 (render #el [hiccup/editor-compiled]))
+(bench :reagent-interpret 10000 (render (r/as-element [hiccup/editor] reagent-compiler)))
