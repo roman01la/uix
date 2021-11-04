ed1b45fd861307616d13bf38878a2130975d3994
roman01la <roman01la@romanliutikov.com>
2021-11-04 16:07:08 +0100

    remove JVM ssr

--- core/test/uix/test_runner.clj
+++ core/test/uix/test_runner.clj
@@ -1,11 +1,9 @@
 (ns uix.test-runner
   (:require [clojure.test :refer :all]
             [uix.core-test]
-            [uix.ssr-test]
             [uix.aot-test]))
 
 (defn -main [& args]
   (run-tests
-    #_'uix.ssr-test
     'uix.core-test
     'uix.aot-test))
