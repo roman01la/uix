82471fe60d6cd1b11763786aa8f3a2a06abb049d
roman01la <roman01la@romanliutikov.com>
2021-11-10 17:11:38 +0200

    fix codegen for zero args components

--- core/src/uix/core.clj
+++ core/src/uix/core.clj
@@ -2,11 +2,20 @@
   "Public API"
   (:require [uix.compiler.aot]))
 
+(defn- no-args-component [sym body]
+  `(defn ~sym []
+     ~@body))
+
+(defn- with-args-component [sym args body]
+  `(defn ~sym [props#]
+     (let [~args (cljs.core/array (glue-args props#))]
+       ~@body)))
+
 (defmacro defui
   "Compiles UIx component into React component at compile-time."
   [sym args & body]
   `(do
-     (defn ~sym [props#]
-       (let [~args (cljs.core/array (glue-args props#))]
-         ~@body))
+     ~(if (empty? args)
+        (no-args-component sym body)
+        (with-args-component sym args body))
      (with-name ~sym)))
