e9f3e11d5f74f5d3f2422f312e2bccc444282255
roman01la <roman01la@romanliutikov.com>
2021-11-23 22:03:18 +0200

    validate UTL in dev

--- core/src/uix/compiler/aot.cljs
+++ core/src/uix/compiler/aot.cljs
@@ -2,10 +2,19 @@
   "Runtime helpers for Hiccup compiled into React.js"
   (:require [react :as react]
             [uix.compiler.alpha :as r]
-            [uix.compiler.attributes]))
+            [uix.compiler.attributes]
+            [uix.lib :refer [doseq-loop]]))
+
+(defn- validate-children [children deep?]
+  (doseq-loop [child children]
+    (cond
+      (and (not deep?) (sequential? child)) (validate-children child true)
+      (vector? child) (throw (js/Error. (str "Hiccup is not valid as UIx child (found: " child "). If you meant to render an element, tag it with #el, i.e. #el " child))))))
 
 (defn >el [tag attrs-children children]
   (let [args (-> #js [tag] (.concat attrs-children) (.concat children))]
+    (when ^goog.DEBUG
+      (validate-children (.slice args 2) false))
     (.apply react/createElement nil args)))
 
 (def suspense react/Suspense)
