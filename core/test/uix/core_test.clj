30c4ba4203cb6a3d6152cc23fe7b45237a0fb34e
roman01la <roman01la@romanliutikov.com>
2021-12-16 15:12:01 +0200

    add tests

--- core/test/uix/core_test.clj
+++ core/test/uix/core_test.clj
@@ -1,5 +1,6 @@
 (ns uix.core-test
   (:require [clojure.test :refer :all]
+            [uix.core]
             [uix.core.lazy-loader :refer [require-lazy]]))
 
 (require-lazy '[clojure.string :refer [blank?]])
@@ -13,3 +14,9 @@
       (macroexpand-1 '(require-lazy '[clojure.string :as str]))
       (catch Exception e
         (is (some? e))))))
+
+(deftest test-parse-sig
+  (is (thrown-with-msg? AssertionError #"uix.core\/defui doesn't support multi-arity"
+                        (uix.core/parse-sig 'component-name '(([props]) ([props x])))))
+  (is (thrown-with-msg? AssertionError #"uix.core\/defui should be a single-arity component"
+        (uix.core/parse-sig 'component-name '([props x])))))
