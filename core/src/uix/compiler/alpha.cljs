e4675bb11d00e40eef506266b7dfea5e950c9d9b
Roman Liutikov <cubexv@gmail.com>
2022-04-12 16:07:43 +0300

    Update core/src/uix/compiler/alpha.cljs
    Co-authored-by: Tibor Claassen <tc@codebeige.net>

--- core/src/uix/compiler/alpha.cljs
+++ core/src/uix/compiler/alpha.cljs
@@ -22,7 +22,7 @@
   (when-not ^boolean (.-uix-component? component-type)
     (let [name-str (or (.-displayName component-type)
                        (.-name component-type))]
-      (throw (js/Error. (str "Invalid use of a non-UIx component " name-str " in `h` form.\n"
+      (throw (js/Error. (str "Invalid use of a non-UIx component " name-str " in `$` form.\n"
                              "If you meant to render React element, use :> syntax for interop with JavaScript components, i.e. ($ :> " name-str ")\n"
                              "If you meant to render Reagent element, make it Hiccup wrapped with r/as-element, i.e. (r/as-element [" name-str "])")))))
   true)
