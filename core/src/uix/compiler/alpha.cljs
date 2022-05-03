6b5cd5c10c9839b6abd366a8e7ebe804e1967b58
roman01la <roman01la@romanliutikov.com>
2022-05-03 16:23:51 +0300

    improve $ error messages

--- core/src/uix/compiler/alpha.cljs
+++ core/src/uix/compiler/alpha.cljs
@@ -21,6 +21,7 @@
     (let [name-str (or (.-displayName component-type)
                        (.-name component-type))]
       (throw (js/Error. (str "Invalid use of a non-UIx component " name-str " in `$` form.\n"
+                             "Make sure that the component is declared with `defui`\n"
                              "If you meant to render React element, use :> syntax for interop with JavaScript components, i.e. ($ :> " name-str ")\n"
                              "If you meant to render Reagent element, make it Hiccup wrapped with r/as-element, i.e. (r/as-element [" name-str "])")))))
   true)
